bda1a0282f788dbd82feefd05a929a3a71bafa09
Guardian <johnsalmans@hotmail.com>
2021-09-20 15:32:44 -0500

    Initial commit

---
+++ src/main/java/me/guardiandev/bylawinfact/ByLawInFact.java
@@ -0,0 +1,16 @@
+package me.guardiandev.bylawinfact;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+public class ByLawInFact implements ModInitializer, ClientModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
